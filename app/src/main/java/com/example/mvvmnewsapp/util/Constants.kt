d9ec5cd907b2f78c8750af34908d941177afc115
Kamil Paczos <61743455+kpaczos@users.noreply.github.com>
2022-01-17 18:34:35 +0100

    Update Constants.kt

--- app/src/main/java/com/example/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/example/mvvmnewsapp/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.mvvmnewsapp.util
 class Constants {
     companion object{
-        const val API_KEY = "63711bbe521d44c6a12bdddbe5c4eec1"
+        const val API_KEY = My API key
         const val BASE_URL ="https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
